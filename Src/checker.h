3545e9711630f93018dcea50fa20e392497939e1
felpsribeiro <felipemch14@gmail.com>
2022-12-31 17:37:52 -0300

    struct contador

--- Src/checker.h
+++ Src/checker.h
@@ -1,11 +1,24 @@
 #ifndef COMPILER_CHECKER
 #define COMPILER_CHECKER
 
+#include <string>
+using namespace std;
 #include "ast.h"
 
-void Tab();
+string Tab();
 string ConvertType(int);
 void TestLexer();
 void TestParser(Node *);
+
+struct InstructionCounter
+{
+    unsigned int block;
+    unsigned int loop;
+    unsigned int depth;
+
+    InstructionCounter();
+    string Tab();
+    string Tab(unsigned int d);
+};
 
 #endif
